3db3e5818d7d0cddc7aae0511c9d28e0e91bb1fb
Patrick Jansky <patrick.jansky@gmx.ch>
2024-08-12 16:21:06 +0200

    Oopsie

--- main.c
+++ main.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 int main(void) {
-<<<<<<< HEAD
-	printf("Very cool branched program\n");
-	return 0;
-=======
-	printf("Very cool program\n");
+	printf("I did a merge oopsie\n");
 	return 69;
->>>>>>> 4ce8c49 (Nice)
 }
