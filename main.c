e05199ff6115a6127b35f56a8f2fd9fe97ef92b9
Patrick Jansky <patrick.jansky@gmx.ch>
2024-08-12 16:17:20 +0200

    Merge NewBranch

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(void) {
+<<<<<<< HEAD
 	printf("Very cool branched program\n");
 	return 0;
+=======
+	printf("Very cool program\n");
+	return 69;
+>>>>>>> 4ce8c49 (Nice)
 }
